b9ff5f4a69c865508ebc85aab0fec79fd890eadb
stuckform <75707524+stuckform@users.noreply.github.com>
2021-01-15 09:58:26 -0500

    commit

---
+++ js/site.js
@@ -0,0 +1,63 @@
+document.getElementById("btnSubmit").addEventListener("click", SunsetHills);
+document.getElementById("btnClear").addEventListener("click", Clear);
+
+
+function SunsetHills() {
+    //1: collect data
+    let num1 = parseInt(document.getElementById("num1").value);
+    let num2 = parseInt(document.getElementById("num2").value);
+    let num3 = parseInt(document.getElementById("num3").value);
+    let num4 = parseInt(document.getElementById("num4").value);
+    let num5 = parseInt(document.getElementById("num5").value);
+    //2: Turn the user data into an array
+  let userArray = [num1, num2, num3, num4, num5]
+
+  // 3:create an ascending array using a for loop
+    let highestBld = userArray[0];
+    let sunSet = [];
+    sunSet.push(highestBld);
+    
+    for(let i=1; i < userArray.length; i++){
+        if(userArray[i] > highestBld){
+            highestBld = userArray[i]
+            sunSet.push(highestBld);
+        }
+    }
+
+    document.getElementById("results").innerText =`The buildings that will see the sunset are[${sunSet.join(',')}]`;
+}
+function Clear(){
+document.getElementById("results").innerText = "";
+document.getElementById("num1").value = "";
+document.getElementById("num2").value = "";
+document.getElementById("num3").value = "";
+document.getElementById("num4").value = "";
+document.getElementById("num5").value = "";
+ 
+
+    
+}
+
+
+
+
+// // sunset function
+// function Sunset(buildings) {
+  
+//   // first building can see the sunset
+//   let results = ['See']
+//   for (let i = 1; i < buildings.length, i++) {
+//     let bigger = false;
+
+//     for (let s = 0; s < i; s++){
+     
+//       if (buildings[s]) >= buildings[i]) {
+        
+//         bigger = true;
+//         break;
+//       } else {
+//         continue
+//       };
+//     }
+//   }
+// }
